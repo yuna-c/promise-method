1adaae2b4fef4d79fd6ea2eb4e732643269bafa4
yuna-c <yuna744@gmail.com>
2023-11-17 10:39:34 +0900

    1117 Promise의 기본 개념

--- js/common.js
+++ js/common.js
@@ -1,34 +1,19 @@
-//
+/*
+ES6 Promise라는 동기화 처리하기 위한 특별한 생성자 함수가 추가
+Promise는 일반 인스턴스 객체와는 달리 생성 시점에 값이 정해지는 것이 아닌 상태값 3가지를 품은 상태에서 반환됨
+추후에 상태값 변환단계에 따라 각각 등록된 메서드가 동기적으로 실행되는 방식
+
+Promise의 3가지 상태
+[pending] : 특정 요청이 들어간 이후 그 요청이 수행될 떄까지의 '대기'상태
+[fullfiled] : 요청이 성공적으로 수행 완료 된 상태
+[rejected] : 요청이 실패해서 수행 완료된 상태
+
+Promise 인스턴스를 생성해야 하는 대표적인 경우
+- [외부 데이터]를 가지고 올 때까지 다음 업무를 holding 시키면서 동기화 처리해야 하는 경우
+- 코드 내부적으로 setTimeout같은 [지연시간이 발생하는 코드]인 경우 해당 코드가 끝날 때까지 holding하면서 동기화 처리
+*/
 function test(delay, callback) {
 	setTimeout(() => {
 		callback();
 	}, delay);
 }
-
-// -start => 2초 후 test1 => 또 2초 후 test => end
-// ES5에서 callback을 통해 동기화 하는 원리
-// 애초에 동기화시키고 싶은코드를 모두 callback형태로 묶어 통채로 web api에게 넘김으로써 callstack입장에서 ㄷ음에 실행할 코드들을 원천적으로 제거하는 방법
-
-// 순서 1 - start출력(callstack)
-console.log('start');
-// 순서 2 - 첫번째 함수 호출(callstack)
-// 순서 3 - setTimeout을 web api에게 전달(callstack)
-test(2000, () => {
-	// 순서 4 - setTimeout호출시 안쪽 구문을 다시 callstack에 전달(wep api)
-	// 순서 5 - web api로 부터 setTimeout안쪽의 callstack함수 전달받고 호출(callstack)
-	console.log('💕');
-	// 순서 6 - 다시 두번재 함수 호출하고 안쪽의 setTimeout을 다시 web api로 전달(callstack)
-	test(2000, () => {
-		// 순서 7 - setTimeout을 수행하고 아래쪽의 callback으로 전달된 구문을 다시 callstack에 전달(web api)
-		// 순서 8 - 아래 구문을 실행하고 종료 (callstack)
-		console.log('💕');
-		console.log('end');
-	});
-});
-
-/* 
-문제점 !
-- 가독성 떨어짐(콜백지옥)
-해결법 !
-- promise
-*/
