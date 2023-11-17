bd650314b13a5751a9c4cd63e6b9dc9459d1e6dd
yuna-c <yuna744@gmail.com>
2023-11-17 11:09:17 +0900

    1117 Promise를 통한 동기화 처리

--- js/common.js
+++ js/common.js
@@ -11,9 +11,29 @@ Promise의 3가지 상태
 Promise 인스턴스를 생성해야 하는 대표적인 경우
 - [외부 데이터]를 가지고 올 때까지 다음 업무를 holding 시키면서 동기화 처리해야 하는 경우
 - 코드 내부적으로 setTimeout같은 [지연시간이 발생하는 코드]인 경우 해당 코드가 끝날 때까지 holding하면서 동기화 처리
+
+Promise란 3가지 내부 상태값을 가지고 있는 객체, 각 객체의 내부 상태 변화에 따른 특정 메서드의 호출을 약속받은 객체
+
+Promise생성자 함수를 통해서 동기화 처리 할 코드의 호출 시점을 메서드로 Promise객체 인스턴트 형태로 리턴
+이때 Promise생성자 내부에서 자동으로 res, rej라는 2개의 인수(함수)가 전달되고
+해당 함수를 전달받은 코드가 동기적으로 수행되게 하고 싶은 시점에 res를 호출(then 호출시점)
+전달받은 코드를 기반으로 동기적으로 에러를 반환하고 싶은 시점에 rej호출(catch)
 */
-function test(delay, callback) {
-	setTimeout(() => {
-		callback();
-	}, delay);
+
+function delay(time, isSucess) {
+	//delay내부에서 생성되는 promise인스턴스를 다시 delay함수 외부로 리턴
+	//delay함수뒤에 바로 then구문을 호출하기 위함
+	//then을 이어서 호출하기 위해서는 이전함수가 promise인스턴스를 반환해야됨
+	return new Promise((res, rej) => {
+		setTimeout(() => {
+			console.log('특정로직이 성공적으로 수행되었을떄 실행할 구문');
+			// isSucess상태에 따라 res, rej가 분기 호출
+			isSucess ? res('sucess💜') /* fullfiled 실행시점 */ : rej('fail🖤') /* rejected 실행 시점 */;
+		}, time);
+	});
 }
+// promise의 변화되는 상태에 따라 then, catch 동기적으로 호출 가능
+const p2 = delay(2000, false)
+	.then((data) => console.log(data))
+	.catch((err) => console.error(err));
+// delay(2000).then(() => console.log('동기적으로 실행할 코드💕'));
